df3c0245e665f62e308eb35ba632cba67ec5ec54
Abinash Panda <abinash@prodios.com>
2021-09-08 14:29:49 +0530

    feat(setup): add vite plugins

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import checker from 'vite-plugin-checker'
+import svgrPlugin from 'vite-plugin-svgr'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
+  plugins: [
+    reactRefresh(),
+    checker({ typescript: true }),
+    svgrPlugin(),
+    tsconfigPaths(),
+  ],
 })
