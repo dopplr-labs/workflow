f64cc860c4d78076d98d5b87e26d45951c7e5599
Himanshu Nagrath <33781906+hnagrath09@users.noreply.github.com>
2021-11-05 23:18:22 +0530

    routes and query client setup at App level

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,20 @@
 import React from 'react'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
 import AllIssues from 'pages/all-issues'
+import Login from 'pages/login/login'
 
 export default function App() {
-  return <AllIssues />
+  const queryClient = new QueryClient()
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/all-issues" element={<AllIssues />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Navigate to="/login" replace={true} />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  )
 }
