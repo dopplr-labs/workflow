ebe4147909e9c6c8432c954b688e1ef354b84189
Himanshu Nagrath <33781906+hnagrath09@users.noreply.github.com>
2021-11-05 19:57:00 +0530

    added nestjs/swagger to auto generate spec.yaml

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,20 @@
+import { resolve } from 'path'
+import { writeFileSync } from 'fs'
 import { NestFactory } from '@nestjs/core'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 import { AppModule } from './app.module'
+
+const yaml = require('js-yaml')
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  const config = new DocumentBuilder().setTitle('Workflow API').build()
+  const document = SwaggerModule.createDocument(app, config)
+  const outputPath = resolve(process.cwd(), 'spec.yaml')
+  writeFileSync(outputPath, yaml.dump(document), 'utf8')
+  SwaggerModule.setup('api', app, document)
+
   await app.listen(3001)
 }
 bootstrap()
