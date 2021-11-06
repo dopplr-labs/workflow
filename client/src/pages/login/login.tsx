dd4416b3c33a3580b223154a8224fb6cdc0aed6d
Himanshu Nagrath <33781906+hnagrath09@users.noreply.github.com>
2021-11-06 16:36:07 +0530

    added validation in login form

--- client/src/pages/login/login.tsx
+++ client/src/pages/login/login.tsx
@@ -11,7 +11,7 @@ export default function Login() {
   }
 
   return (
-    <div className="flex items-center justify-center w-screen h-screen bg-white">
+    <div className="flex items-center justify-center w-screen h-screen bg-gray-50">
       <div className="flex flex-col items-center w-full max-w-md p-4 space-y-4">
         <div className="flex items-center justify-center w-12 h-12 text-2xl font-semibold text-white bg-blue-600 rounded-lg">
           W
@@ -22,36 +22,56 @@ export default function Login() {
           </p>
           <p className="text-sm text-center">
             Or{' '}
-            <Link to="/sign-up">
-              <span className="text-blue-600 text-semibold">
-                create a new account
-              </span>
+            <Link
+              to="/sign-up"
+              className="font-medium text-blue-600 hover:text-blue-700"
+            >
+              create a new account
             </Link>
           </p>
         </div>
         <Form
-          className="w-full"
-          wrapperCol={{ span: 6 }}
+          className="w-full px-8 py-12 bg-white rounded shadow-md"
           onSubmit={signInWithEmail}
           defaultValues={{ rememberMe: true }}
+          layout={Form.Layout.VERTICAL}
         >
-          <Form.Item name="email">
-            <Input placeholder="Email address" />
-          </Form.Item>
-          <Form.Item name="password">
-            <Input type="password" placeholder="Password" />
-          </Form.Item>
-          <div className="flex items-center justify-between w-full">
-            <Form.Item name="rememberMe" valuePropName="checked">
-              <Checkbox label="Remember me" />
-            </Form.Item>
-            <p className="text-sm text-blue-800">Forgot your password?</p>
-          </div>
-          <Form.Item>
-            <Button className="w-full" type="submit" buttonType="primary">
-              Submit
-            </Button>
-          </Form.Item>
+          {({ isValid }: { isValid: boolean }) => (
+            <>
+              <Form.Item
+                name="email"
+                label="Email address"
+                rules={[
+                  { required: true, message: 'Please enter your email!' },
+                ]}
+              >
+                <Input />
+              </Form.Item>
+              <Form.Item
+                name="password"
+                label="Password"
+                rules={[
+                  { required: true, message: 'Please enter your password!' },
+                ]}
+              >
+                <Input type="password" />
+              </Form.Item>
+              <div className="flex items-center justify-between w-full">
+                <Form.Item name="rememberMe" valuePropName="checked">
+                  <Checkbox label="Remember me" />
+                </Form.Item>
+                <p className="text-sm text-blue-800">Forgot your password?</p>
+              </div>
+              <Button
+                className="w-full"
+                type="submit"
+                buttonType="primary"
+                disabled={!isValid}
+              >
+                Submit
+              </Button>
+            </>
+          )}
         </Form>
       </div>
     </div>
